bdc19822cd10759139a8c722585a47db8856e9aa
hirotomoriyama-g <hiroto.moriyama7204@gmail.com>
2021-03-31 13:11:38 +0900

    jQueryを導入

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-$(function(){
-  $('.box1').slideDown(function(){
-  $('.box1').css({
-    'background-color':'#0000ff',
-    'width':'200px',
-    'height':'100px'
-  }).slideUp();
-  });
+$(document).ready(function(){
+  $('body').html('<h1>Hello jQuery!!</h1>');
 });
